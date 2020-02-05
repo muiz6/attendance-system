adb43626c84470bc6e422cbd4de560028b4d4035
muiz6 <muizhassan83@gmail.com>
2020-02-05 10:33:40 +0500

    init

---
+++ src/main/java/com/muiz6/attendancesystem/Main.java
@@ -0,0 +1,12 @@
+package com.muiz6.attendancesystem;
+
+// import org.joda.time.LocalTime;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+
+        // LocalTime time = new LocalTime();
+        // System.out.println("time: "+time);
+    }
+}
---
+++ src/main/java/com/muiz6/attendancesystem/ui/UI.java
@@ -0,0 +1,9 @@
+package com.muiz6.attendancesystem.ui;
+
+import javax.swing.JFrame;
+
+public class UI {
+    UI() {
+        JFrame frame;
+    }
+}
