26eb9efad5da3218eb3fededcb9238235e9659b2
muiz6 <muizhassan83@gmail.com>
2020-02-09 14:25:32 +0500

    javafx init

--- src/main/java/com/muiz6/attendancesystem/MyApp.java
+++ src/main/java/com/muiz6/attendancesystem/MyApp.java
@@ -2,7 +2,7 @@ package com.muiz6.attendancesystem;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
@@ -16,10 +16,11 @@ public class MyApp extends Application{
 
         WebView webView = new WebView();
 
-        webView.getEngine().load("http://google.com");
+        webView.getEngine().load(this.getClass().getResource("/html/login.html").toString());
 
-        VBox vBox = new VBox(webView);
-        Scene scene = new Scene(vBox, 960, 600);
+        StackPane stack = new StackPane();
+        stack.getChildren().add(webView);
+        Scene scene = new Scene(stack);
 
         primaryStage.setScene(scene);
         primaryStage.show();
