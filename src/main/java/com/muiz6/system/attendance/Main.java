d7b6f3ba3728e09fccc1387f4514ba82e8f01855
muiz6 <muizhassan83@gmail.com>
2020-06-02 18:54:08 +0500

    created basic side navigation bar

--- src/main/java/com/muiz6/system/attendance/Main.java
+++ src/main/java/com/muiz6/system/attendance/Main.java
@@ -3,8 +3,7 @@ package com.muiz6.system.attendance;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -20,14 +19,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle(Strings.APPLICATION_TITLE);
 
+		// load navigation panel from fxml
 		final FXMLLoader loader = new FXMLLoader();
 		// get gradle resource
 		URL url = ClassLoader.getSystemClassLoader()
 				.getResource("layout/panel_navigation.fxml");
 		loader.setLocation(url);
-		final BorderPane root = loader.load();
+		final Pane root = loader.load();
 
 		primaryStage.setScene(new Scene(root, 800, 600));
+		primaryStage.setMinWidth(800);
+		primaryStage.setMinHeight(600);
 		primaryStage.setMaximized(true);
 		primaryStage.show();
 	}
--- src/main/java/com/muiz6/system/attendance/ui/NavigationPanel.java
+++ src/main/java/com/muiz6/system/attendance/ui/NavigationPanel.java
@@ -1,9 +1,21 @@
 package com.muiz6.system.attendance.ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
-public class NavigationPanel {
-	
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavigationPanel implements Initializable {
+
+	@FXML
+	private StackPane _stackPane;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		_stackPane.getChildren().add(new Button("Dynamic Button"));
+	}
 }
