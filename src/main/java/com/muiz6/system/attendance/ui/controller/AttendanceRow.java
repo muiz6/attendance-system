667322c8f10c522f1b5fb0cb5fe8cfa49c9b54a2
muiz6 <muizhassan83@gmail.com>
2020-06-06 14:49:49 +0500

    added alert feature when absent is marked

--- src/main/java/com/muiz6/system/attendance/ui/controller/AttendanceRow.java
+++ src/main/java/com/muiz6/system/attendance/ui/controller/AttendanceRow.java
@@ -3,24 +3,30 @@ package com.muiz6.system.attendance.ui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.text.MessageFormat;
 
 public class AttendanceRow {
 
+	private int _id;
+	private String _name;
+	private long _joinDate;
 	@FXML
 	private GridPane _gridPane;
 	@FXML
 	private ToggleButton _btnAttendance;
 
 	public void set(byte id, String name, long joinDate) {
+		_id = id;
+		_name = name;
+		_joinDate = joinDate;
 		try {
 			final URL url = ClassLoader.getSystemClassLoader()
 					.getResource("layout/col_employee.fxml");
@@ -38,14 +44,28 @@ public class AttendanceRow {
 		}
 	}
 
-	// public void onAttendanceBtnClick(ActionEvent actionEvent) {
-	// 	// button is just pressed and selected
-	// 	if (_btnAttendance.isSelected()) {
-	// 		_btnAttendance.setText("P");
-	// 	}
-	// 	else {
-	//
-	// 		_btnAttendance.setText("A");
-	// 	}
-	// }
+	public void onAttendanceBtnClick(ActionEvent actionEvent) {
+		// button is just pressed and selected
+		if (_btnAttendance.isSelected()) {
+			_btnAttendance.setText("P");
+		}
+		else {
+			String msg = MessageFormat.format("Mark Absent of {0}?",
+					_name);
+			Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+					msg,
+					ButtonType.NO,
+					ButtonType.YES);
+			alert.setTitle("Mark Absent");
+			alert.showAndWait();
+
+			if (alert.getResult() == ButtonType.YES) {
+				_btnAttendance.setText("A");
+			}
+			else {
+				// restore state
+				_btnAttendance.setSelected(true);
+			}
+		}
+	}
 }
