501c70d377655ebb286a97e59c03cf23984e7936
muiz6 <muizhassan83@gmail.com>
2020-06-07 14:40:38 +0500

    created ui for add employee form

--- src/main/java/com/muiz6/system/attendance/ui/controller/AddEmployeeContent.java
+++ src/main/java/com/muiz6/system/attendance/ui/controller/AddEmployeeContent.java
@@ -1,4 +1,55 @@
 package com.muiz6.system.attendance.ui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
 public class AddEmployeeContent {
+
+	@FXML
+	private Button _btnTimeSame;
+	@FXML
+	private Button _btnTimeMonday;
+	@FXML
+	private Button _btnTimeTuesday;
+	@FXML
+	private Button _btnTimeWednesday;
+	@FXML
+	private Button _btnTimeSunday;
+	@FXML
+	private Button _btnTimeSaturday;
+	@FXML
+	private Button _btnTimeFriday;
+	@FXML
+	private Button _btnTimeThursday;
+	@FXML
+	private RadioButton _btnRadioDifferent;
+	@FXML
+	private RadioButton _btnRadioSame;
+
+	public void onOptionSelect(ActionEvent event) {
+		Button[] arr = {
+				_btnTimeMonday,
+				_btnTimeTuesday,
+				_btnTimeWednesday,
+				_btnTimeThursday,
+				_btnTimeFriday,
+				_btnTimeSaturday,
+				_btnTimeSunday};
+		Object source = event.getSource();
+		if (source == _btnRadioSame && _btnRadioSame.isSelected()) {
+			_btnTimeSame.setDisable(false);
+			for (final Button i: arr) {
+				i.setDisable(true);
+			}
+		}
+		else if (source == _btnRadioDifferent
+				&& _btnRadioDifferent.isSelected()) {
+			_btnTimeSame.setDisable(true);
+			for (final Button i: arr) {
+				i.setDisable(false);
+			}
+		}
+	}
 }
